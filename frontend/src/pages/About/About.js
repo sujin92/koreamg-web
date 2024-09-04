828eb808d32c7e4e28e1d06de92a807e00215b62
Sujin Lim <920907vv@gmail.com>
2024-09-04 15:59:28 +0900

    Remove submodule and add frontend folder as a regular folder

---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about">
+      <h1>회사소개</h1>
+      <p>이곳에 회사 소개에 대한 내용을 작성합니다.</p>
+    </div>
+  );
+}
+
+export default About;
